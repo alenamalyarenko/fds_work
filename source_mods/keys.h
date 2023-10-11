d37111aff0a9e50efa94379d7f69cbfdc8022ed3
Alena Malyarenko alena.malyarenko@canterbury.ac.nz <ama677@maui01.maui.niwa.co.nz>
2023-10-11 16:40:38 +1300

    cleanup

--- source_mods/keys.h
+++ source_mods/keys.h
@@ -1,7 +1,10 @@
-#define temperature_init_in
 
-#undef velocity_init_in
-#undef temperature_ic
-#undef velocity_in
-#undef plot_3d_out
-#undef plot_hrr_out
+#if defined coupled
+ #define global_mesh
+ #define temperature_init_in
+ #undef velocity_init_in
+ #undef temperature_ic
+ #undef velocity_in
+ #undef plot_3d_out
+ #undef plot_hrr_out
+#endif
