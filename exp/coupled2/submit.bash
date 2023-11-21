4705f03ecddd02d4de1bec2d0795f8cef8756412
alenamalyarenko <ama677@maui01.maui.niwa.co.nz>
2023-11-21 13:54:13 +1300

    coupled2

--- exp/coupled2/submit.bash
+++ exp/coupled2/submit.bash
@@ -3,8 +3,8 @@
 #SBATCH --time           00:10:00       #Walltime
 #SBATCH --job-name       fds
 ###SBATCH --qos            debug
-#SBATCH --account        vuw03158
-#SBATCH --ntasks         32              #One task per mesh, NO MORE
+#SBATCH --account        scion03617
+#SBATCH --ntasks         9              #One task per mesh, NO MORE
 #SBATCH --hint           nomultithread  #Hyperthreading decreases efficiency.
 ###SBATCH --mem=512MB          # Memory in MB
 #SBATCH --output=%x-%j.out    # %x and %j are replaced by job name and ID
