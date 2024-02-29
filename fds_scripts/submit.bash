ec2a18ac52567d82d11e381467f18878a9203b42
Alena Malyarenko <alena.malyarenko@canterbury.ac.nz>
2024-02-29 14:35:06 +1300

    scripts cleanup

--- fds_scripts/submit.bash
+++ fds_scripts/submit.bash
@@ -1,18 +1,17 @@
 #!/bin/bash -e
 
-#SBATCH --time           00:10:00       #Walltime
+#SBATCH --time           00:05:00       #Walltime
 #SBATCH --job-name       fds
-###SBATCH --qos            debug
-#SBATCH --account        vuw03158
+#SBATCH --qos            debug
+#SBATCH --account        scion03617
 #SBATCH --ntasks         1              #One task per mesh, NO MORE
 #SBATCH --hint           nomultithread  #Hyperthreading decreases efficiency.
-###SBATCH --mem=512MB          # Memory in MB
+#SBATCH --mem=10000MB          # Memory in MB
 #SBATCH --output=%x-%j.out    # %x and %j are replaced by job name and ID
 #SBATCH --error=%x-%j.err
 
 ##SBATCH --mail-type=BEGIN,END,FAIL
 ##SBATCH --mail-user=alena.malyarenko@canterbury.ac.nz
 
-input="/nesi/project/vuw03158/fds/fds/exp/grass/Input/Level_Set_2m_multi_mesh.fds"
 
-srun read
+srun bc.exe
